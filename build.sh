7f5897a91c8bce820256a5de6087fa015c7f41d6
tsukudamayo <tsukudamayo@gmail.com>
2021-01-12 08:10:26 +0000

    add cassava-leaf-disease-classification/cassava-leaf-disease-classification.yml

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+xhost +local:
 docker run \
-    -it \
-    --rm \
+    -it --rm \
     --gpus all \
     -v $(pwd):/workspace \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
     torch-gpu-py3.8 \
     /bin/bash
-
+xhost -local:
