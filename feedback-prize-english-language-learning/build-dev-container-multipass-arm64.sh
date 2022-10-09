da0132103c43f1400e0119a201283889e33f466c
tsukudamayo <tsukudamayo@gmail.com>
2022-10-09 08:04:36 +0000

    create new projects

---
+++ feedback-prize-english-language-learning/build-dev-container-multipass-arm64.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export DOCKER_BUILDKIT=1
+
+docker build -t kaggle -f ./feedback-prize-english-language-learning/dev.arm64.Dockerfile .
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    xhost +$(multipass list | grep docker-vm | awk '{print $3}')
+    docker run -it --rm \
+        -v /tmp:/tmp \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e DISPLAY=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}'):0.0 \
+        --name feedback-prize-english-language-learning \
+        kaggle \
+        /bin/bash
+    xhost -$(multipass list | grep docker-vm | awk '{print $3}')
+else
+    xhost +local:
+    docker run -it --rm \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e DISPLAY=$DISPLAY \
+        --name feedback-prize-english-language-learning \
+        kaggle \
+        /bin/bash
+    xhost -local:
+fi
